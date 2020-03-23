a947b68cef4dd42fae1f33379bac042fc4c9cb36
Carlos Garcia <gtxcarl9@gmail.com>
2020-03-23 17:23:53 +0100

    Creacted blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Blog = () => {
+    return(
+        <div>
+            <h1>Blog</h1>
+            <h2>Full s stack web developer, living in beatiful Luanda</h2>
+        </div>
+    )
+}
+
+export default Blog
