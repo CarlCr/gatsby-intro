843a69d6569d9864a604851a43b0ad0837173c0b
Carlos Garcia <gtxcarl9@gmail.com>
2020-03-23 17:24:03 +0100

    Creacted contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>Contact</h1>
+            <h2>Full s stack web developer, living in beatiful Luanda</h2>
+            <p> contact me via <a href="https://github.com/carlcr" target="_blank"></a></p>
+        </div>
+    )
+}
+
+export default Contact
