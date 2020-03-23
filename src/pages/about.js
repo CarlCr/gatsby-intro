a9a0e5e508ade868fa81b072dac50e3f12ea65c5
Carlos Garcia <gtxcarl9@gmail.com>
2020-03-23 17:23:34 +0100

    Creacted   about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+const About = () => {
+    return(
+        <div>
+            <h1>About</h1>
+            <h2>Full s stack web developer, living in beatiful Luanda</h2>
+            <p><Link to="/contact"></Link> contact me</p>
+        </div>
+    )
+}
+
+export default About
