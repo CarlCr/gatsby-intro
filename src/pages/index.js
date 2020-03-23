725b04ab1f2a72b39cbed987746efa513c45a703
Carlos Garcia <gtxcarl9@gmail.com>
2020-03-23 17:24:12 +0100

    Creacted main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Home = () => {
     return(
         <div>
             <h1>Hi Iam Carlos Garcia</h1>
             <h2>Full s stack web developer, living in beatiful Luanda</h2>
+            <p>Need a developer <Link to="/contact">Contact me </Link></p>
         </div>
     )
 }
